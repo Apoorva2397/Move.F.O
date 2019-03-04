14c7efd7bc19e50b36250167bd2bc68efd7e4e02
Apoorva2397 <39225063+Apoorva2397@users.noreply.github.com>
2019-03-04 13:03:48 +0530

    march 2019

---
+++ Assets/Laser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+   private Vector2 start;
+   private Vector2 End;
+   CompletePlayerController PlayerController;
+    public void Start()
+    {
+        //PlayerController = gameObject.GetComponent<CompletePlayerController>();
+    }
+    private void Awake()
+    {
+        
+    }
+    // Use this for initialization
+    
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        Debug.DrawLine(start, End, Color.red);
+
+    }
+    private void Update()
+    {
+       RaycastHit2D hit = Physics2D.Raycast(start, Vector2.left, 50.0f);
+        if (hit.transform.gameObject.name == "Player")
+            Destroy(hit.transform.gameObject);
+            PlayerController.killPlayer(3);
+    }
+}
---
+++ Assets/_Completed-Assets/Scripts/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Utils : MonoBehaviour {
+    private static int currentLevel;
+    private void Awake()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+    // Update is called once per frame
+    public static void LoadNextLevel () {
+        SceneManager.LoadScene(currentLevel + 1);
+        	}
+    public static void reloadCurrentScene() {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
